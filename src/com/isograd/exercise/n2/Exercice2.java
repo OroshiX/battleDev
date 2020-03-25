758e3eba1e726e1a7e4e7ca1ff3ce2a325e0b5eb
OroshiX <oroshix@users.noreply.github.com>
2020-03-25 22:15:33 +0100

    BattleDev 2020 March 26 Preparation

--- src/com/isograd/exercise/n2/Exercice2.java
+++ src/com/isograd/exercise/n2/Exercice2.java
@@ -1,23 +1,10 @@
 package com.isograd.exercise.n2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Exercice2 {
     public void solve(int testNumber, Scanner sc, PrintWriter out) {
-
-        List<Integer> planches = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            planches.add(sc.nextInt());
-        }
-        Integer min = planches.stream().min(Integer::compareTo).get();
-        final int[] sum = {0};
-        planches.forEach(integer -> {
-            sum[0] += integer -min;
-        });
-
-        System.out.println(sum[0]);
+        System.out.println(sc.nextInt());
     }
 }
--- src/com/isograd/exercise/n3/Exercice3.java
+++ src/com/isograd/exercise/n3/Exercice3.java
@@ -4,50 +4,7 @@ import java.util.*;
 import java.io.PrintWriter;
 
 public class Exercice3 {
-    public static void main(String[] argv) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        List<Integer> resCable = new ArrayList<>(M);
-        List<StartEnd> startEnds = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            startEnds.add(new StartEnd(sc.nextInt(), sc.nextInt()));
-        }
-        int min = startEnds.stream().min(Comparator.comparing(o -> o.start)).get().start;
-        int max = startEnds.stream().max(Comparator.comparing(o -> o.end)).get().end;
-        int[] usageCables = new int[max - min];
-
-        for (int i = 0; i < M; i++) {
-            StartEnd startEnd = startEnds.get(i);
-            int maxNumCable = 0;
-            for (int j = startEnd.start - min; j < startEnd.end - min; j++) {
-                usageCables[j]++;
-                if (usageCables[j] > maxNumCable) {
-                    maxNumCable = usageCables[j];
-                }
-                if (usageCables[j] > N) {
-                    System.out.println("pas possible");
-                    return;
-                }
-//                resCable.add(usageCables[j]);
-            }
-            resCable.add(maxNumCable);
-        }
-        StringJoiner sj = new StringJoiner(" ");
-        resCable.forEach(integer -> sj.add(integer.toString()));
-        System.out.println(sj.toString());
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+        System.out.println(2);
     }
-
-
-}
-
-class StartEnd {
-    public StartEnd(Integer start, Integer end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    public Integer start, end;
-
 }
